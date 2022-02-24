4c17b633fe65cc83c3db0accc10036e84890ab8a
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-02-24 11:07:25 +0100

    add model & migration categories

---
+++ database/migrations/2022_02_24_100126_update_categories_plates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateCategoriesPlatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('plates', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->after('id')->nullable();
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories')
+                  ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('plates', function (Blueprint $table) {
+            $table->dropForeign('plates_category_id_foreign');
+            $table->dropColumn('category_id');
+        });
+    }
+}
