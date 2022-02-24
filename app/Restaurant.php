f3f1be27523c4316dd323c37d08a597c5eba8de2
FedericoTrimarco <trimarcofederico98@gmail.com>
2022-02-24 10:50:29 +0100

    add models & migrations restaurants, typologies

---
+++ app/Restaurant.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function typology(){
+        return $this->belongsTo('App\Typology');
+    }
+}
---
+++ database/migrations/2022_02_24_092441_create_restaurants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->string('vat_number', 20);
+            $table->text('cover')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
