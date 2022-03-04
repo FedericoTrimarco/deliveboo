1c490815ff98cc6fa448bb863efc256cc2b68973
luirusso <luirusso_@hotmail.it>
2022-03-04 12:31:57 +0100

    ADD: aside template

---
+++ resources/js/components/Aside.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <div class="row">
+            <aside class="col-3">
+
+            </aside>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Aside',
+}
+</script>
+
+<style scoped style="scss">
+
+</style>
