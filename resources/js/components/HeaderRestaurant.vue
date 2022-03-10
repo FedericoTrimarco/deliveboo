03f57caaac0a4bfd70b3d105f334ab29ffc7956c
artemisia000 <misiapierobon@gmail.com>
2022-03-10 18:28:53 +0100

    responsive

--- resources/js/components/HeaderRestaurant.vue
+++ resources/js/components/HeaderRestaurant.vue
@@ -2,26 +2,40 @@
     <header>
         <div class="container">
             <div class="row">
-                <div class="col-4">
+
+                <div class="col-12 col-md-6 col-lg-4">
                     <img :src="image" alt="">    
                 </div>
-                <div class="col-6 ps-4">
+
+                <div class="d-sm-none d-md-block col-md-6 col-lg-6 ps-4">
                     <h2>{{ name }} </h2>
-                    <p class="mt-4" ><span>Categorie:</span>Americana</p>
-                    <p><span>Ci troviamo in:</span>{{ address }}</p>
+                    <p class="mt-4">
+                        <span><i class="fa-solid fa-utensils icon me-2"></i>Categorie:
+                        </span>
+                        Americana
+                    </p>
+
+                    <p>
+                        <span><i class="fa-solid fa-map-pin icon me-2"></i>Ci troviamo in:
+                        </span>
+                        {{ address }}
+                    </p>
+
                     <div class="box-info" >
                         <i class="fa-solid fa-info icon"></i>
                         <a href=""
                            class="info">
-                            <span class="ms-2 me-2" >Informazioni</span>
+                            <span>Informazioni</span>
                         </a>
                         <i class="fa-solid fa-chevron-right icon"></i>
                     </div>
                 </div>
-                <div class="col-2">
+
+                <div class="d-none d-sm-block d-sm-none d-md-block d-md-none d-lg-block col-lg-2">
                     <div>
                         <i class="fa-solid fa-person-biking icon"></i>
-                        <span>Cosegna tra: 40-50 minuti</span>
+                        <span>Consegna fra:</span>
+                        <div class="mt-3" >40/50 minuti</div>
                     </div>
                 </div>
             </div>
@@ -39,6 +53,7 @@ export default {
         name: String,
         address: String,
     }
+
 }
 </script>
 
@@ -53,6 +68,7 @@ export default {
            width: 100%;
            height: 265px;
            border-radius: 5px;
+           box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
        }
        h2{
            font-size: 45px;
@@ -68,7 +84,7 @@ export default {
            justify-content: space-between;
            padding: 8px;
            &:hover{
-               border: 2px solid #ffb703;
+               border: 4px solid #f5dd9f;
            }
            .info{
                text-decoration: none;
@@ -80,6 +96,10 @@ export default {
            font-size: 18px;
            color: #ffb703;
           
+       }
+
+       .content{
+           width: 150px;height: 80px;
        }
 
    }
